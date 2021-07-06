1f57179a14bacab79482122fa4f309031ed4bb3e
Prashant Jha <prashant.jha@walmart.com>
2021-07-06 19:30:53 +0530

    initial UI setup

--- app/src/main/java/com/numerify/ui/home/HomeFragment.kt
+++ app/src/main/java/com/numerify/ui/home/HomeFragment.kt
@@ -4,28 +4,43 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.numerify.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpViewsListeners()
+        observerViewModels()
+    }
+
+    private fun setUpViewsListeners() {
+        textInputField.editText?.doOnTextChanged { inputText, _, _, _ ->
+            //deal with the text
+        }
+    }
+
+    private fun observerViewModels() {
+        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+
+        })
     }
 }
